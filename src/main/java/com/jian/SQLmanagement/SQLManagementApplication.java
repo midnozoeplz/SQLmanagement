0219d8cb9b1d4fd31d1c2059be4de7cdcee9979f
Seraphine丶 <romantic0906@qq.com>
2022-05-21 13:45:44 +0800

    create environment

---
+++ src/main/java/com/jian/SQLmanagement/SQLManagementApplication.java
@@ -0,0 +1,18 @@
+package com.jian.SQLmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jianz
+ * @version 1.0
+ * @Description
+ * @Email jianz8153.gmail.com
+ * @Date 2022/5/21 13:33
+ */
+@SpringBootApplication
+public class SQLManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SQLManagementApplication.class,args);
+    }
+}
